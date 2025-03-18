9bbad06d8a389a27203e57cfef35a462c133ab83
Andrey <kandreym96@outlook.com>
2025-03-17 20:12:32 -0500

    SRPC v1.0.2

--- app.py
+++ app.py
@@ -71,5 +71,5 @@ def get_data():
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080, threads=8)
+    serve(app, host="0.0.0.0", port=8080, threads=16)
 
